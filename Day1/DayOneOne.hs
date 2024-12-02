023f5828276960800c7333d3c562f37ab0aebcc3
Nick Test <grigoriannick@gmail.com>
2024-12-02 03:06:52 -0500

    Found some optimizations for Day One part One

--- Day1/DayOneOne.hs
+++ Day1/DayOneOne.hs
@@ -2,13 +2,12 @@ import System.IO
 import Data.List
 
 main = do
-    contents <- readFile "inputOne.txt"
-    let l1 = sort . fst . unzip . map readInts $ lines contents
-    let l2 = sort . snd . unzip . map readInts $ lines contents
-    print ( sum $ map diff $ zip l1 l2)
+    contents <- readFile "input.txt"
+    let (l1, l2) = unzip . map readInts $ lines contents
+    print ( sum $ zipWith diff l1 l2)
     
 readInts :: String -> (Int, Int)
-readInts s = (read (words s !! 0), read (words s !! 1))
+readInts s = (read (head (words s)), read (words s !! 1))
 
-diff :: (Int, Int) -> Int
-diff (i1, i2) = abs $ i1 - i2
+diff :: Int -> Int -> Int
+diff i1 i2 = abs $ i1 - i2
